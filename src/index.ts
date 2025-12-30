3abea4bd6a174b5aae92eebf64c8c4a16d96f09c
ultraspaceeng <ultraspaceeng@gmail.com>
2025-12-31 00:29:43 +0100

    Add files via upload

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import { searchAndScrape } from './scraper';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8001;
 
 app.use(cors());
 app.use(express.json());
